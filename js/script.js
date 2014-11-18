8bfe726847d73e80f736af1c907b0659723f37fb
Сергей <tovarov.piter@gmail.com>
2014-11-18 18:36:58 +0400

    main-menu, basic layout + main layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $(function(){
 	
-	
+	// work with menu
+
+	$('.menu-open, .menu-close').on('click', function(){
+		$('.main-menu').toggleClass('active');
+		$('body').toggleClass('hidden')
+	})
+
+
+	// tab control
+
+	var tabs = $('.tab-control').find('a');
+
+	tabs.on('click',function(){
+		tabs.removeClass('active');
+		$(this).addClass('active');
+
+		$('.tab').removeClass('active');
+		$('.' + $(this).attr('data-tab') ).addClass('active');
+
+	})
 
 });
